3d286a656af6d90d53e2b681ed36eff21b0deced
Carlos <cxhiva@gmial.com>
2024-06-15 20:31:38 +0200

    change to ollamaEmbeddings

--- db/chroma.py
+++ db/chroma.py
@@ -1,5 +1,6 @@
 from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
+
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
@@ -10,9 +11,8 @@ docs = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+vectorstore = Chroma.from_documents(documents=splits, embedding=OllamaEmbeddings())
 
-# Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
 
 
