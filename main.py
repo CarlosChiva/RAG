e9441fadc6755a3feedb2f0fabd51ab2eccea8d4
CosminIordache <cosminionutiordache@gmail.com>
2024-06-15 19:37:10 +0200

    simple structure api

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import *
+from routes.routes import router
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- routes/routes.py
+++ routes/routes.py
@@ -1,2 +1,13 @@
 from fastapi import *
+from controllers import controllers
 
+router = APIRouter()
+
+@router.get("/")
+async def index():
+    result = await controllers.index()
+
+    if "error" in result:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=result["error"])
+    
+    return result
