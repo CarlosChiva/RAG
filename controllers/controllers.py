98fe5d60d71715c385a7642f5ac7cb1dadd53f40
carlos <cxhiva@gmail.com>
2024-07-07 19:19:33 +0200

    add create collections and show

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,4 +1,8 @@
 
-
+from db import saver
 async def index():
     return {"message": "Hello World"}
+
+async def new_collection(documents):
+    maker= await saver.create_new_collection(collection_name="first", pdf_path=documents)
+    return maker
--- db/saver.py
+++ db/saver.py
@@ -1,17 +1,58 @@
+import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
+from langchain.document_loaders import PyPDFLoader
 
-from langchain.document_loaders.pdf import PyPDFLoader
+embedding_func = OllamaEmbeddings()
 
-def load_pdf(filename):
+def __load_pdf(filename):
     loader = PyPDFLoader(filename)
     documents = loader.load()
     return documents
-MODEL = "llama2"
-data="path of the pdf"
-document=load_pdf(data)
-text_splitter= RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
-all_splits = text_splitter.split_documents(document)
+def __text_split(documents):
+   text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+   all_splits = text_splitter.split_documents(documents)
+   return all_splits
+async def create_new_collection(collection_name, pdf_path):
+    documents = __load_pdf(pdf_path)
+    splits=__text_split(documents)
+    vectorstore = Chroma.from_documents(
+        documents=splits, 
+        embedding=embedding_func,
+        persist_directory=f"db1/{collection_name}",
+        collection_name=collection_name
+    )
+    vectorstore.persist()
+    print(f"Nueva colección '{collection_name}' creada con éxito.")
+    return {"message": f"Nueva colección '{collection_name}' creada con éxito."}
 
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=OllamaEmbeddings(),persist_directory="db")
+def add_documents_to_collection(collection_name, pdf_path):
+    documents = __load_pdf(pdf_path)
+    splits=__text_split(documents)
+    vectorstore = Chroma(
+        persist_directory=f"db1/{collection_name}",
+        embedding_function=embedding_func,
+        collection_name=collection_name
+    )
+    vectorstore.add_documents(documents=splits)
+    vectorstore.persist()
+    print(f"Documentos añadidos a la colección '{collection_name}' con éxito.")
+
+def __get_collections():
+    base_directory = "db1"
+    if not os.path.exists(base_directory):
+        return []
+    
+    collections = [name for name in os.listdir(base_directory) 
+                   if os.path.isdir(os.path.join(base_directory, name))]
+    return collections
+
+def show_collections():
+    collections = __get_collections()
+    if collections:
+        print("Colecciones existentes:")
+        for collection in collections:
+            print(f"- {collection}")
+    else:
+        print("No hay colecciones existentes.")
