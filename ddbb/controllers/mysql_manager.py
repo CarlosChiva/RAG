3b1745dad4895ffe12fad07388833e9583fd8ad5
Dread <cxhiva@gmail.com>
2025-02-02 12:26:46 +0100

    little refactor of connections links between containers

--- ddbb/controllers/mysql_manager.py
+++ ddbb/controllers/mysql_manager.py
@@ -5,7 +5,8 @@ import os
 async def db_connect():
     db = mysql.connector.connect(
     
-        host=os.getenv("DB_HOST"),         user=os.getenv("DB_USER"),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         port=3306,
         database=os.getenv("DB_DATABASE")
