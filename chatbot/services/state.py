0d3a09f2e1e8708ac1884a33c713bfb2c4cc512c
Dread <cxhiva@gmail.com>
2025-04-24 20:43:22 +0200

    first look to graph adding image-generator node

--- chatbot/services/state.py
+++ chatbot/services/state.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 from typing import Any, Optional, Literal
 from typing_extensions import TypedDict
-
+from langgraph.graph.message import add_messages
+from typing import Annotated
 
 class Config(TypedDict):
     model: str
