e10e2fe028915f16f0ac2af2a85870f3edf8a0f8
Dread <cxhiva@gmail.com>
2025-05-19 18:56:29 +0200

    add same functionality to save conversations than rag_PDF

--- chatbot/controllers/controllers.py
+++ chatbot/controllers/controllers.py
@@ -4,7 +4,7 @@ from services.graph_service import graph
 from services.ollama_services import get_models
 from config import Config
 from langchain_core.messages import HumanMessage
-from chats_controller import new_conversation
+from controllers.chats_controller import new_conversation
 async def query(credentials,conf:Config):
     #invoke grafo(input,conf_id_conversation,configuracion_modelo)
     config = {
