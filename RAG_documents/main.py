a15e6a03566272dd93b9ee90245fe359665ff10b
Dread <cxhiva@gmail.com>
2025-05-24 19:19:23 +0200

    fix little issue in chatoutput in rag ddbb

--- RAG_documents/main.py
+++ RAG_documents/main.py
@@ -15,5 +15,5 @@ app.add_middleware(
 )
 app.include_router(router)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
