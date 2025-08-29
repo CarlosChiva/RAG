72b8a89490b3e9514ae2d1affcddc91473bede60
Dread <cxhiva@gmail.com>
2025-08-29 18:04:27 +0200

    add websocket connection with comfyui

--- chatbot/services/nodes/nodes.py
+++ chatbot/services/nodes/nodes.py
@@ -118,8 +118,62 @@ def queue_prompt(prompt):
 
 
     data = json.dumps(p).encode('utf-8')
-    req =  request.Request("http://localhost:8188/prompt", data=data)
+    req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
     request.urlopen(req)
+
+
+import websocket #NOTE: websocket-client (https://github.com/websocket-client/websocket-client)
+import uuid
+import json
+import urllib.request
+import urllib.parse
+
+server_address = "127.0.0.1:8188"
+client_id = str(uuid.uuid4())
+
+def queue_prompt(prompt):
+    p = {"prompt": prompt, "client_id": client_id}
+    data = json.dumps(p).encode('utf-8')
+    req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
+    return json.loads(urllib.request.urlopen(req).read())
+
+def get_image(filename, subfolder, folder_type):
+    data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
+    url_values = urllib.parse.urlencode(data)
+    with urllib.request.urlopen("http://{}/view?{}".format(server_address, url_values)) as response:
+        return response.read()
+
+def get_history(prompt_id):
+    with urllib.request.urlopen("http://{}/history/{}".format(server_address, prompt_id)) as response:
+        return json.loads(response.read())
+
+def get_images(ws, prompt):
+    prompt_id = queue_prompt(prompt)['prompt_id']
+    output_images = {}
+    current_node = ""
+    while True:
+        out = ws.recv()
+        if isinstance(out, str):
+            message = json.loads(out)
+            if message['type'] == 'executing':
+                data = message['data']
+                if data['prompt_id'] == prompt_id:
+                    if data['node'] is None:
+                        break #Execution is done
+                    else:
+                        current_node = data['node']
+        else:
+            if current_node == 'save_image_websocket_node':
+                images_output = output_images.get(current_node, [])
+                images_output.append(out[8:])
+                output_images[current_node] = images_output
+
+    return output_images
+
+
+
+
+
 
 async def image_generator(state:MessagesState,config):
     websocket = config["configurable"].get("websocket")
@@ -129,11 +183,20 @@ async def image_generator(state:MessagesState,config):
     logging.info(f"image_prompt---{config["configurable"]["userInput"]}    {config["configurable"]["tools"]["image_tools"]["api_json"]}  {config["configurable"]["tools"]["image_tools"]["positive_prompt_node"]}")
 
 
-    # prompt = json.loads(config["configurable"]["tools"]["image_tools"]["api_json"])
-    # #set the text prompt for our positive CLIPTextEncode
-    # prompt[str(config["configurable"]["tools"]["image_tools"]["positive_prompt_node"])]["inputs"]["text"] = config["configurable"]["userInput"]
+    prompt = config["configurable"]["tools"]["image_tools"]["api_json"]
+    #set the text prompt for our positive CLIPTextEncode
+    prompt[str(config["configurable"]["tools"]["image_tools"]["positive_prompt_node"])]["inputs"]["text"] = config["configurable"]["userInput"]
+    try:
 
-    # queue_prompt(prompt)
-
+        ws = websocket.WebSocket()
+        ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
+        images = get_images(ws, prompt)
+        ws.close() # for in case this example is used in an environment where it will be repeatedly called, like in a Gradio app. otherwise, you'll randomly receive connection timeouts
+        websocket.send_json({
+            "event": "Image generated",
+            "images": images
+        })
+    except Exception as e:
+        logging.error(f"Error sending websocket message: {e}")
 
     pass
